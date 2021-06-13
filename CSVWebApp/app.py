d28ca58cccf709b1ace810874aebcc7bf80c4f87
PeterX12 <peteroluc@gmail.com>
2021-06-13 20:33:28 +0100

    Templates and Css made

--- CSVWebApp/app.py
+++ CSVWebApp/app.py
@@ -6,24 +6,34 @@ import csv
 import io
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
+import warnings
+warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
+db_path = os.path.join(os.path.dirname(__file__), 'database.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['ALLOWED_EXTENSIONS'] = set(['csv'])
+app.secret_key = "temp for dev"
+
+db = SQLAlchemy(app)
+
+# If db file does not exist...
 file = os.path.isfile("./database.db")
-# If db file exists...
-if file:
+if not file:
+    import sqlite3
+    from app import db  
+    conn = sqlite3.connect('database.db')
+    conn.close()
+    db.create_all()
     db_path = os.path.join(os.path.dirname(__file__), 'database.db')
     db_uri = 'sqlite:///{}'.format(db_path)
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['ALLOWED_EXTENSIONS'] = set(['csv'])
-    app.secret_key = ";fd;jf;ldsjf;lsdjgdfajgfajgp 'ierajgweug'iagjr'u"
+    app.secret_key = "temp for dev"
+
     db = SQLAlchemy(app)
-# If db file does not exist...
-else:
-    conn = sqlite3.connect('database.db')
-    conn.close()
-    from app import db 
-    db.create_all()
     
 # Classes
 class TheFile(db.Model):
@@ -32,14 +42,40 @@ class TheFile(db.Model):
     data = db.Column(db.LargeBinary)
 
 # Functions
-def file_check(file):
-    return '.' in file and \
-        file.rsplit('.',1)[1] in app.config['ALLOWED_EXTENSIONS'] 
+def file_check(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 # Home Page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Upload Page
+@app.route('/upload_page')
+def upload_page():
+    return render_template('upload.html')
+
+# Uploading the CSV file
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the file to be uploaded
+    selected_file = request.files['file']
+    # Check if the file is a CSV file and is valid
+    if file and file_check(selected_file.filename):
+        filename = secure_filename(selected_file.filename)
+        new_file = TheFile(name=filename, data=selected_file.read())
+        db.session.add(new_file)
+        db.session.commit()
+        return redirect(url_for('library'))
+    else:
+    	return redirect(url_for('error'))
+
+# Library of the uploaded CSV Files
+@app.route('/library')
+def library():
+	library = TheFile.query.all()
+	return render_template('library.html', title="CSV Files", library=library)
+
+if __name__ == '__main__':
+	app.run(debug=True)
